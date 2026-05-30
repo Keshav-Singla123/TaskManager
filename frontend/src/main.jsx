5fbeb54340abda6e6c080845cfa77fe5fb128684
Keshav Singla <keshavsingla701@gmail.com>
2026-05-30 19:48:45 +0530

    feat(frontend): add entry point

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
+import { TaskProvider } from "./context/TaskContext";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <TaskProvider>
+          <App />
+        </TaskProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
